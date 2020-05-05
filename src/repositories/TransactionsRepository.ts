123ddc6552109936e32abeda9052de1037743d9d
Pedro Felipe Dominguite <pfdominguite@gmail.com>
2020-05-04 22:18:13 -0300

    Add transactions repository, routes, and service

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -9,20 +9,31 @@ interface Balance {
 class TransactionsRepository {
   private transactions: Transaction[];
 
+  private balance: Balance;
+
   constructor() {
     this.transactions = [];
+    this.balance = { income: 0, outcome: 0, total: 0 };
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    return this.balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: Omit<Transaction, 'id'>): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+    if (type === 'income') {
+      this.balance.income += value;
+    } else {
+      this.balance.outcome += value;
+    }
+    this.balance.total += type === 'income' ? value : -value;
+    return transaction;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -8,8 +8,24 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Omit<Transaction, 'id'>): Transaction {
+    if (type !== 'income' && type !== 'outcome') {
+      throw Error(
+        "Invalid 'type' for request: should be 'income' or 'outcome'.",
+      );
+    }
+    if (
+      value > this.transactionsRepository.getBalance().total &&
+      type === 'outcome'
+    ) {
+      throw Error('Insufficient balance to complete operation.');
+    }
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+    return transaction;
   }
 }
 
